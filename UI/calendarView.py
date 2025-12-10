85f289079c95c0421289b9fc74f110eddb9e9598
dodonocik <dodonocik@gmail.com>
2025-12-10 01:37:00 +0100

    przeniesiono ui do osobnego folderu, submit button zamyka modal (dialog)

---
+++ UI/calendarView.py
@@ -0,0 +1,81 @@
+import streamlit as st
+from datetime import date, datetime
+from streamlit_calendar import calendar
+from zoneinfo import ZoneInfo
+
+def runCalendarView() -> None:
+    st.set_page_config(page_title="Demo for streamlit-calendar", page_icon="📆")
+
+
+
+    events = [
+    {
+        "title": "Event 1",
+        "color": "#FF6C6C",
+        "start": "2025-12-01",
+        "end": "2025-12-11",
+    },
+    ]
+
+    calendar_options = {
+        "navLinks": "true",
+        "selectable": "true",
+        "height": "auto",
+        "contentHeight": "auto",
+        "expandsRows": "true",
+        "slotMinTime": "00:00:00",
+        "slotMaxTime": "23:59:59",
+        "allDaySlot": False,
+    "headerToolbar": {
+            "left": "prev,today,next",
+            "center": "title",
+            "right": "dayGridDay,dayGridWeek,dayGridMonth",
+        },
+        "initialDate": date.today().isoformat(),
+        "initialView": "dayGridMonth",
+    }
+
+
+    state = calendar(
+        events=st.session_state.get("events", events),
+        options=calendar_options,
+        custom_css="""
+        .fc-event-past {
+            opacity: 0.8;
+        }
+        .fc-event-time {
+            font-style: italic;
+        }
+        .fc-event-title {
+            font-weight: 700;
+        }
+        .fc-toolbar-title {
+            font-size: 2rem;
+        }
+        """,
+    )
+
+    dt = datetime.now()
+    dt_pl = datetime.now()
+
+    if state and "dateClick" in state:
+            dt = datetime.fromisoformat(state["dateClick"]["date"])
+            dt_pl = dt.astimezone(ZoneInfo("Europe/Warsaw"))
+
+    @st.dialog("Wybierz cel")
+    def choose(choosendate):
+        with st.form("goal"):
+            date_val = st.date_input("Deadline:", choosendate.date())
+            title_val = st.text_input("Nazwa:", "Nazwij swoj cel")
+            desc_val = st.text_input("Opis:", "Opisz swoj cel")
+            priority_val = st.selectbox("Priorytet", ["malo wazne", "srednio wazne", "bardzo wazne"])
+            submitted = st.form_submit_button("Dodaj")
+            if submitted:
+    #WYSŁANIE FORMA DO API
+                print(date_val, title_val, desc_val, priority_val)
+                #ZAMKNIECIE DIALOGU PO KLINIECIU SUBMIT
+                st.session_state["show_dialog"] = False
+                st.rerun()
+    if "choose" not in st.session_state:
+        if st.button("Dodaj cel"):
+            choose(dt_pl)
